03ad06e2bec7cd53744822551a506acf444ae7ae
Maya11011 <maya.shterenberg@gmail.com>
2025-08-19 11:56:13 +0300

    eTl first part

--- dags/etl_dag.py
+++ dags/etl_dag.py
@@ -23,27 +23,172 @@ with DAG(
     tags=['trino', 'iceberg', 'hive', 'dedup', 'hash']
 ) as dag:
 
-    insert_new_payment_methods = TrinoOperator(
-        task_id='insert_new_payment_methods',
+    dim_payment_methods = TrinoOperator(
+        task_id='dim_payment_methods',
         sql="""
-            INSERT INTO iceberg.analytics.payment_methods_summary (
+            INSERT INTO iceberg.analytics.dim_payment_methods (
                 method_name,
                 code
             )
             SELECT
-                pm.payment_method AS method_name,
+                pm.transactionType AS method_name,
                 xxhash64(pm.payment_method) AS code
             FROM (
-                SELECT DISTINCT payment_method
+                SELECT DISTINCT transactionType
                 FROM hive.payme.payments
                 WHERE load_date >= DATE '{{ ds }}'
                   AND load_date < DATE '{{ next_ds }}'
             ) pm
             WHERE NOT EXISTS (
                 SELECT 1
-                FROM iceberg.analytics.payment_methods_summary existing
-                WHERE existing.method_name = pm.payment_method
+                FROM iceberg.analytics.dim_payment_methods existing
+                WHERE existing.method_name = pm.transactionType
             );
         """,
         trino_conn_id=trino_conn_id  # <-- Dynamically set from Variable
     )
+
+    dim_currency = TrinoOperator(
+        task_id='dim_currency',
+        sql="""
+            INSERT INTO iceberg.analytics.dim_currency (
+            currency_name,
+            code,
+            exchange_rate
+            )
+            SELECT
+                new_currencies.currency AS currency_name,
+                xxhash64(new_currencies.currency) AS code,
+                rates.rate AS exchange_rate
+            FROM (
+                SELECT DISTINCT currency, paymentDate
+                FROM hive.payme.payments
+                WHERE load_date >= DATE '{{ ds }}'
+                AND load_date < DATE '{{ next_ds }}'
+            ) new_currencies
+            LEFT JOIN (
+                SELECT currency, rate, rate_date
+                FROM hive.boi.exchange_rates
+            ) rates
+            ON new_currencies.currency = rates.currency
+            AND new_currencies.paymentDate = rates.rate_date
+            WHERE NOT EXISTS (
+                SELECT 1
+                FROM iceberg.analytics.dim_currency existing
+                WHERE existing.currency_name = new_currencies.currency
+            );
+        """,
+        trino_conn_id=trino_conn_id
+    )
+
+
+    dim_user = TrinoOperator(
+    task_id='dim_user',
+    sql="""
+        INSERT INTO iceberg.analytics.dim_user (
+            userId
+        )
+        SELECT DISTINCT fb.userId
+        FROM hive.flight.bookings fb
+        WHERE fb.userId IS NOT NULL
+        AND NOT EXISTS (
+            SELECT 1
+            FROM iceberg.analytics.dim_user existing
+            WHERE existing.userId = fb.userId
+        );
+    """,
+    trino_conn_id=trino_conn_id
+)
+
+
+    dim_time = TrinoOperator(
+    task_id='dim_time',
+    sql="""
+        WITH date_range AS (
+            SELECT
+                DATE_ADD('month', -1, DATE(min(booking_date))) AS start_date,
+                DATE '{{ ds }}' AS end_date
+            FROM hive.flight.bookings
+        ),
+        calendar AS (
+            SELECT
+                DATE_ADD('day', x, start_date) AS full_date
+            FROM date_range,
+                 UNNEST(SEQUENCE(0, DATE_DIFF('day', start_date, end_date))) AS t(x)
+        )
+        INSERT INTO iceberg.analytics.dim_time (
+            date_key,
+            full_date,
+            year,
+            month_num,
+            month_name,
+            week_of_year,
+            week_label,
+            week_start_date,
+            weekday_num,
+            weekday_name,
+            is_weekend
+        )
+        SELECT
+            CAST(DATE_FORMAT(full_date, '%Y%m%d') AS INTEGER) AS date_key,
+            full_date,
+            YEAR(full_date),
+            MONTH(full_date),
+            DATE_FORMAT(full_date, '%M') AS month_name,
+            WEEK_OF_YEAR(full_date),
+            FORMAT('%s-W%s', YEAR(full_date), LPAD(CAST(WEEK_OF_YEAR(full_date) AS VARCHAR), 2, '0')) AS week_label,
+            DATE_TRUNC('week', full_date) AS week_start_date,
+            DAY_OF_WEEK(full_date),
+            DATE_FORMAT(full_date, '%W') AS weekday_name,
+            CASE WHEN DAY_OF_WEEK(full_date) IN (6, 7) THEN TRUE ELSE FALSE END AS is_weekend
+        FROM calendar
+        WHERE NOT EXISTS (
+            SELECT 1
+            FROM iceberg.analytics.dim_time existing
+            WHERE existing.date_key = CAST(DATE_FORMAT(full_date, '%Y%m%d') AS INTEGER)
+        );
+    """,
+    trino_conn_id=trino_conn_id
+)
+    
+
+stg_payments = TrinoOperator(
+    task_id='stg_payments',
+    sql="""
+        INSERT INTO iceberg.staging.stg_payments (
+            payment_id,
+            booking_id,
+            user_id,
+            payment_method_code,
+            currency_code,
+            base_currency,
+            paid_date_key,
+            paymentDate,
+            amount,
+            price_in_base_currency
+        )
+        SELECT
+            -- SK based on user_id + paid_date
+            xxhash64(p.user_id || DATE_FORMAT(DATE(p.paymentDate), '%Y-%m-%d')) AS payment_id,
+            b.booking_id,
+            p.user_id,
+            pm.code AS payment_method_code,
+            dc.code AS currency_code,
+            'ILS' AS base_currency,
+            CAST(DATE_FORMAT(p.paymentDate, '%Y%m%d') AS INT) AS paid_date_key,
+            p.paymentDate AS paid_at_timestamp,
+            p.amount,
+            p.amount * COALESCE(dc.exchange_rate, 1) AS price_in_base_currency
+        FROM hive.payme.payments p
+        LEFT JOIN hive.flight.bookings b
+            ON p.user_id = b.user_id
+           AND DATE(p.paid_at) = DATE(b.booking_date)
+        LEFT JOIN iceberg.analytics.payment_methods_summary pm
+            ON pm.method_name = p.payment_method
+        LEFT JOIN iceberg.analytics.dim_currency dc
+            ON dc.currency_name = p.currency
+        WHERE p.load_date >= DATE '{{ ds }}'
+          AND p.load_date < DATE '{{ next_ds }}';
+    """,
+    trino_conn_id=trino_conn_id
+)    
